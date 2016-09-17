64cb94adbee648e9551271debf69622e12701b5e
Per Lundholm <per@artcomputer.se>
2016-09-17 10:34:28 +0200

    Updates are shown. The picture URL of the user is stored along the message.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,10 +3,13 @@ var theFormInput = document.getElementById("theForm-input");
 var signInButton = document.getElementById('sign-in-button');
 var signOutButton = document.getElementById('sign-out-button');
 var splashPage = document.getElementById('page-splash');
+var recentPostsSection = document.getElementById('recent-posts-list');
+var listeningFirebaseRefs = [];
 
-function addPost(text) {
+function addPost(text, picture) {
     var postData = {
-        message: text
+        message: text,
+        authorPic: picture
     };
 
     // Get a key for a new Post.
@@ -20,12 +23,57 @@ function addPost(text) {
 
 var currentUID;
 function cleanupUi() {
-
+    console.log("Clean up");
+    listeningFirebaseRefs.forEach(function (ref) {
+        ref.off();
+    });
+    listeningFirebaseRefs = [];
 }
+
+function createPostElement(message, authorPic) {
+    var html = '<div class="content">' +
+        '<span class="avatar"></span><span class="text"></span>' +
+        '</div>';
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    var postElement = div.firstChild;
+
+    postElement.getElementsByClassName('text')[0].innerText = message;
+    postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
+        (authorPic || './silhouette.jpg') + '")';
+    return postElement;
+}
+
+function startDatabaseQueries() {
+    console.log("Starting database queries");
+    var fetchPosts = function (postsRef, sectionElement) {
+        postsRef.on('child_added', function (data) {
+            console.log("Child added");
+            var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+            containerElement.insertBefore(
+                createPostElement(data.val().message, data.val().authorPic),
+                containerElement.firstChild);
+        });
+    };
+
+    var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
+    fetchPosts(recentPostsRef, recentPostsSection);
+    listeningFirebaseRefs.push(recentPostsRef);
+}
+
+function writeUserData(uid, displayName, email, photoURL) {
+    firebase.database().ref('users/' + uid).set({
+        username: displayName,
+        email: email,
+        profile_picture: photoURL
+    });
+}
+
 /**
  * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
  */
 function onAuthStateChanged(user) {
+    console.log("Change auth state");
     // We ignore token refresh events.
     if (user && currentUID === user.uid || !user && currentUID === null) {
         return;
@@ -64,7 +112,7 @@ window.addEventListener('load', function () {
         e.preventDefault();
         var text = theFormInput.value;
         if (text) {
-            addPost(text);
+            addPost(text, firebase.auth().currentUser.photoURL);
         }
         theFormInput.value = '';
     };
